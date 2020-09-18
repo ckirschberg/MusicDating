da736d51c3dcc1e557292553bb97305ddae53c18
Christian Kirschberg <ckirschberg@gmail.com>
2020-09-18 15:59:20 +0200

    changed Instrument namespace

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MusicDating.Models.Entities;
 
 namespace MusicDating.Data
 {
--- Models/Entities/Instrument.cs
+++ Models/Entities/Instrument.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace MusicDating 
+namespace MusicDating.Models.Entities 
 {
     public class Instrument 
     {
