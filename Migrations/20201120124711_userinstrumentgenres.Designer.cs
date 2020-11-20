5a47f6d4b9a770fca2c5bb7c31aaebd41cbd04f0
Christian Kirschberg <ckirschberg@gmail.com>
2020-11-20 14:10:35 +0100

    .

--- Migrations/20201120124711_userinstrumentgenres.Designer.cs
+++ Migrations/20201120124711_userinstrumentgenres.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MusicDating.Data;
 
 namespace MusicDating.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201120124711_userinstrumentgenres")]
+    partial class userinstrumentgenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20201120124711_userinstrumentgenres.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicDating.Migrations
+{
+    public partial class userinstrumentgenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInstrumentGenres",
+                columns: table => new
+                {
+                    UserInstrumentGenreId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Id = table.Column<string>(nullable: true),
+                    InstrumentId = table.Column<int>(nullable: false),
+                    GenreId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInstrumentGenres", x => x.UserInstrumentGenreId);
+                    table.ForeignKey(
+                        name: "FK_UserInstrumentGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserInstrumentGenres_UserInstruments_Id_InstrumentId",
+                        columns: x => new { x.Id, x.InstrumentId },
+                        principalTable: "UserInstruments",
+                        principalColumns: new[] { "Id", "InstrumentId" },
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "d6f0d856-f63b-4657-8de0-44ad693f70b5", "2ef45e92-a94d-4063-aa9f-2dbf5b0b0cb6" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "7ae3e3d8-73b4-4017-9ecc-734360c0699c", "50576ec4-3cdd-40a9-b3cb-9e531e674bb4" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInstrumentGenres_GenreId",
+                table: "UserInstrumentGenres",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserInstrumentGenres_Id_InstrumentId",
+                table: "UserInstrumentGenres",
+                columns: new[] { "Id", "InstrumentId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInstrumentGenres");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "129814ff-40ba-4dde-a09f-e5c4a463624d", "a9169e85-4392-4b12-b70b-ace7ea5c50b0" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "2",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "22d51e8f-9dce-483b-aedd-924b15f819f2", "95468be8-1fe1-40b7-8313-7689c1d35c29" });
+        }
+    }
+}
